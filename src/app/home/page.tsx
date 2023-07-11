0bc8f1f8d8185943ab0a7461ebd6e2c392279a5c
Manuel Miglioranza <mmiglioranza22@gmail.com>
2023-07-11 14:35:17 -0300

    radio button with text added for filter

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,8 +2,14 @@
 import Link from 'next/link'
 import styles from './page.module.css'
 import Button from '@/components/Button'
+import ApiClient from '@/utils/apiClient'
+import { useState } from 'react'
+
 
 export default function Home() {
+
+const [filter, setFilter] = useState<string>('')
+const [query, setQuery] = useState<string>('')
 
   const ROUTES = [
     'films',
@@ -15,17 +21,37 @@ export default function Home() {
   ]
 
   const navLinks =  ROUTES.map((el: string) => (
-    <Button className={styles.button} key={el}>
-      {/* <Link href={`/${el}`}> */}
-        {el.toUpperCase()}
-      {/* </Link> */}
-    </Button>
+    // <Button className={styles.button} key={el}>
+    //   {/* <Link href={`/${el}`}> */}
+    //     {el.toUpperCase()}
+    //   {/* </Link> */}
+    // </Button>
+<div key={el}>
+  <input type="radio" name="filter" id={el} value={el} />
+  <label htmlFor={el}>{el.toUpperCase()}</label>
+</div>
   ))
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e.preventDefault()
     // eslint-disable-next-line no-console
     console.log({e: e.target})
+    const response = await ApiClient.request('films')
+
+    // eslint-disable-next-line no-console
+    console.log({response})
+
+    
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+
+    setQuery(e.target.value)
+
+  }
+
+  const handleClick = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setFilter(e.target.value)
   }
 
   return (
@@ -35,7 +61,7 @@ export default function Home() {
           <nav className={styles.links}>
             {navLinks} 
           </nav>
-          <input className={styles.input} placeholder='Search...' type="text" />
+          <input className={styles.input} onChange={handleChange} value={query} placeholder='Search...' type="text" />
         </form>
       </div>
     </main>
