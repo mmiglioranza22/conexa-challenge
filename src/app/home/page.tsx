e9fc2a79b0330434c0bcbd03d1d6ed1ccc894b19
Manuel Miglioranza <mmiglioranza22@gmail.com>
2023-07-12 17:00:10 -0300

    fetch requests working and loading state added

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,23 +1,28 @@
 'use client'
-import { useState } from 'react'
+import { useState, useMemo } from 'react'
 import styles from './page.module.css'
 import RadioInput from '@/components/RadioInput'
 import Button from '@/components/Button'
+import Loading from './loading'
+import { SwapiResponse } from '@/types/types'
+// import { CardContainer } from '@/components/CardContainer'
 
-async function getData(url:string) {
-  const res = await fetch(url)
+async function getData(url:string): Promise<any> {
+  const res = await fetch(url, { cache: 'no-store' })
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
-  return res.json()
+  return await res.json()
 }
 
 export default function Home() {
 
 const [filter, setFilter] = useState<string>('')
 const [query, setQuery] = useState<string>('')
+const [data, setData] = useState<any>()
+const [isLoading, setLoading] = useState<boolean>(false)
 
-  const ROUTES = [
+  const ROUTES: SwapiResponse[] = [
     'films',
     'people',
     'planets',
@@ -36,13 +41,13 @@ const [query, setQuery] = useState<string>('')
     }
 
     // eslint-disable-next-line no-console
-    console.log({url})
-
-    const req = await getData(url)
+    // console.log({url})
+    setLoading(() => true)
+    const data = await getData(url)
+    setData(() => data )
+    setLoading(() => false)
     // eslint-disable-next-line no-console
-    console.log({req})
-
-    
+    console.log({data})
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
@@ -54,36 +59,66 @@ const [query, setQuery] = useState<string>('')
   }
 
   const filterOptions = ROUTES.map((value: string) => {
-   return (
-    <RadioInput 
-      key={value} 
-      checked={value === filter} 
-      value={value} 
-      onChange={() => handleRadio(value)}
-      // onClick={handleSubmit} 
-    />)
+    return (
+      <RadioInput 
+        key={value} 
+        checked={value === filter} 
+        value={value} 
+        onChange={() => handleRadio(value)}
+      />
+    )
   })
 
+  const normalizeData = (data: any, type: string) => {
+    switch(type as SwapiResponse) {
+      case 'films':
+        return (<div>Title: {data.title}, Episode: {data.episode_id}</div>)
+      case 'people':
+      case 'starships':
+      case 'vehicles':
+      case 'species':
+      case 'planets':
+        return (<div>Name: {data.name}</div>)
+    }
+  } 
+
+  const fetchDataMemo = useMemo(() => {
+    return data?.results.map((value: any, i: any) => {
+      return (
+        <div key={i}>
+          {normalizeData(value, filter)}
+        </div>
+      )
+    })
+  }, [data])
+
+  const fetchData = data?.results.map((value: any, i: any) => {
+    const elements = normalizeData(value, filter)
+    return (<div key={i}>{elements}</div>)
+  })
 
   return (
     <main className={styles.main}>
       <div className={styles.nav}>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <div className={styles.links}>
-            <Button type="submit" className={styles.radio_button}>{filterOptions}</Button>
+        <div style={{height: 'auto'}}>
+          <form className={styles.form} method='GET' action='/api' onSubmit={handleSubmit}>
+            <div className={styles.links}>
+              <Button type="submit" className={styles.radio_button}>{filterOptions}</Button>
+            </div>
+            {/* <input
+              type="text"
+              placeholder='Search...'
+              className={styles.input}
+              onChange={handleChange}
+              value={query}
+            /> */}
+          </form>
+          {/* <CardContainer type={filter} className={''}>
+          </CardContainer> */}
+          <div className={styles.card_container}>
+            {isLoading ? <Loading /> : fetchDataMemo}
           </div>
-
-          {/* <input
-            type="text"
-            placeholder='Search...'
-            className={styles.input}
-            onChange={handleChange}
-            value={query}
-          /> */}
-        </form>
-      </div>
-      <div>
-
+        </div>
       </div>
     </main>
   )
---
+++ src/types/types.ts
@@ -0,0 +1,1 @@
+export type SwapiResponse = 'films' | 'people' | 'planets' | 'species' | 'starships' | 'vehicles'
